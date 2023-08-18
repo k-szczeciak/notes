277e5f4d532dc5ed9664009c017c2f4063a0ebfe
Krzysztof Szczeciak <krzysztof.szczeciak@mahr.com>
2023-08-18 16:13:24 +0200

    vault backup: 2023-08-18 16:13:24

--- _LogBook_/2023/2023.06/auto.py
+++ _LogBook_/2023/2023.06/auto.py
@@ -1,25 +1,3 @@
-'''
-import os
-
-
-def list_files_in_current_folder(exclude_extensions=[]):
-    files = os.listdir()
-    filtered_files = [file for file in files if not any(file.endswith(ext) for ext in exclude_extensions)]
-    return filtered_files
-
-def create_text_file_with_list(file_list):
-    with open('file_list.txt', 'w') as f:
-        for filename in file_list:
-            f.write('- [[' + filename + ']]\n')
-
-if __name__ == "__main__":
-    excluded_extensions = ['.py']
-    file_list = list_files_in_current_folder(excluded_extensions)
-    create_text_file_with_list(file_list)
-    print("File list (excluding Python files) has been saved to 'file_list.txt'")
-    
-    '''
-    
 import os
 
 def find_common_prefix(strings):
@@ -40,13 +18,13 @@ def list_files_in_current_folder(exclude_extensions=[]):
     return filtered_files
 
 def create_text_file_with_list(file_list, common_prefix):
-    file_name = common_prefix if common_prefix else "default_name"
-    with open(f'{file_name}_file_list.txt', 'w') as f:
+    file_name = common_prefix if common_prefix else "no_common_prefix"
+    with open(f'{file_name}log.md', 'w') as f:
         for filename in file_list:
-            f.write(filename + '\n')
+            f.write('- [[' + filename + ']]\n')
 
 if __name__ == "__main__":
-    excluded_extensions = ['.py']
+    excluded_extensions = ['.py', '.txt']
     file_list = list_files_in_current_folder(excluded_extensions)
     common_prefix = find_common_prefix(file_list)
     create_text_file_with_list(file_list, common_prefix)
